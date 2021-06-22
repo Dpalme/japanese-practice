ce802b1f13a0f71c66a3b99615e005758f351d6e
Diego Palmerin <57266493+Dpalme@users.noreply.github.com>
2021-06-22 09:28:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ function newWord() {
     var keyword = wordList[Math.floor(Math.random() * wordList.length)];
     var apiurl = 'https://jisho.org/api/v1/search/words?keyword=' + keyword;
     $.get(apiurl, function (data) {
-        clearContent();
         var result = data["data"][0];
         var kanji = result["japanese"][0]["word"];
         var hiragana = result["japanese"][0]["reading"];
@@ -20,71 +19,12 @@ function newWord() {
         var definitions = result["senses"][0]["english_definitions"].join(', ')
         msg.text = romaji.split('\xa0').join('');
 
-
-        objectToContent({
-            type: 'span',
-            class: 'mono title-3',
-            style: 'letter-spacing: 3rem; margin-right: -3rem',
-            innerText: hiragana
-        });
-
-        objectToContent({
-            type: 'div',
-            class: 'title-4 mono mb-1',
-            innerText: romaji
-        });
-
-        // remove strings from objects and prettify
-        objectToContent({
-            type: 'div',
-            class: 'body-text mb-sm',
-            innerText: definitions
-        });
-
-        // remove strings from objects and prettify
-        objectToContent({
-            type: 'p',
-            class: 'small-body mb-1',
-            innerText: senses
-        });
-
-
-        objectToContent({
-            type: 'div',
-            class: 'container mb-1',
-            children: [
-                object({
-                    type: 'span',
-                    class: 'col-h h3',
-                    innerText: kanji ? kanji : "no kanji"
-                }), object({
-                    type: 'span',
-                    class: 'col-h h3',
-                    innerText: katakana
-                })
-            ]
-        });
-
-        objectToContent({
-            type: 'button',
-            class: 'col-4 yellow-btn-bg',
-            innerText: 'speak',
-            onclick: 'javascript:soundWord()'
-        })
-
-        objectToContent({
-            type: 'button',
-            class: 'col-4 yellow-btn-bg',
-            innerText: 'night',
-            onclick: 'javascript:document.body.classList.toggle("night")'
-        })
-        
-        objectToContent({
-            type: 'button',
-            class: 'col-4 blue-btn-bg',
-            innerText: 'new',
-            onclick: 'javascript:newWord()'
-        })
+        $('#hiragana').text(hiragana);
+        $('#romaji').text(romaji);
+        $('#definition').text(definitions);
+        $('#type').text(senses);
+        $('#kanji').text(kanji ? kanji : "no kanji");
+        $('#katakana').text(katakana);
     })
 }
 
